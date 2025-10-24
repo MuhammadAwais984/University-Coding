5ae8ae6df3d4c4721fe5b1bcc6192f647e8da7d9
Muhammad Awais <awaisoo668@gmail.com>
2025-10-24 22:04:04 +0500

    Delete Lab-2 directory

--- Lab-2/Problem-1(4).cpp
+++
@@ -1,5 +0,0 @@
-#include<iostream>
-using namespace std;
- int main() {
-    cout<<"*\t*\n *     *\n  *   *\n    *\n   *  *\n  *    *\n *\t*";
- }
--- Lab-2/Problem-1(5).cpp
+++
@@ -1,6 +0,0 @@
-#include<iostream>
-using namespace std;
-int main(){
-    cout<<"*********\n*\t*\n*\t*\n*\t*\n*********";
-    return 0;
-}
