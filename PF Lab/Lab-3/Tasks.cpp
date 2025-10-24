5cf7c6d04278ab4b11a2adad74693b9b02f38e1f
Muhammad Awais <awaisoo668@gmail.com>
2025-10-24 22:21:48 +0500

    Update Tasks.cpp

--- PF Lab/Lab-3/Tasks.cpp
+++ PF Lab/Lab-3/Tasks.cpp
@@ -1,1 +1,77 @@
 
+#include<iostream>
+using namespace std;
+ int main(){
+ 	/*
+ 	
+ 	----------------------------(1)----------------------------	 
+
+ 	int a,b;
+ 	cout<<"Enter two numbers:";
+ 	cin>>a>>b;
+
+ 	cout<<"Addition of two number is: "<<(a+b)<<endl;
+ 	cout<<"Subtraction of two number is: "<<(a-b)<<endl;
+ 	cout<<"Multiplication of two number  is: "<<(a*b)<<endl;
+ 	cout<<"Divivsion of two number  is: "<<(a/b)<<endl;
+ 	cout<<"Modulus of two number  is: "<<(a%b)<<endl;
+ 	
+ 	----------------------------(2)----------------------------	 
+ 	
+ 	float r,area,diameter,circum,pi=3.1415;
+ 	cout<<"Enter radius of circle:";
+ 	cin>>r;
+ 	
+ 	 diameter=2*(r);
+ 	 area=pi*(r*r);
+	circum=(2*pi*r);
+	cout<<"Diameter is: "<<diameter<<"\nCircumfrence is : "<<circum<<"\nArea is: "<<area<<endl; 
+	
+ 	----------------------------(3)----------------------------	 
+ 	
+ 	int a;
+ 	cout<<"Enter number whose table you want: ";
+ 	cin>>a;
+ 	cout<<a<<" x 1 = "<<a*1<<endl;
+ 	cout<<a<<" x 2 = "<<a*2<<endl;
+ 	cout<<a<<" x 3 = "<<a*3<<endl;
+ 	cout<<a<<" x 4 = "<<a*4<<endl;
+ 	cout<<a<<" x 5 = "<<a*5<<endl;
+ 	cout<<a<<" x 6 = "<<a*6<<endl;
+ 	cout<<a<<" x 7 = "<<a*7<<endl;
+ 	cout<<a<<" x 8 = "<<a*8<<endl;
+ 	cout<<a<<" x 9 = "<<a*9<<endl;
+ 	cout<<a<<" x 10 = "<<a*10<<endl;
+ 	
+ 	----------------------------(4)----------------------------	 
+ 	
+ 	float n_gallons=20;
+ 	float town_avg=23.5;
+ 	float highway_avg=28.9;
+ 	cout<<"In twon distance: "<<(n_gallons*town_avg)<<"\nOut town : "<<(n_gallons*highway_avg)<<endl;
+ 	
+ 	----------------------------(5)----------------------------	 
+ 	
+ 	 int number;
+
+    cout << "Enter a five-digit integer (e.g., 42339): ";
+    cin >> number;
+    
+    int d4 = number / 10000;    
+    int d3 = (number / 1000) % 10;
+    int d2 = (number / 100) % 10;
+    int d1 = (number / 10) % 10;
+    int d0 = number % 10;
+
+    cout << d4 << "   " 
+         << d3 << "   " 
+         << d2 << "   " 
+         << d1 << "   " 
+         << d0 << endl;
+ 	
+ 	*/
+ 	
+ 	
+ 	
+ 	
+ }
