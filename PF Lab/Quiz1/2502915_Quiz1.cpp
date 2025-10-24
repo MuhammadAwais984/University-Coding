417ec6b246d52c3504cfba2de13b78e8c7738e9a
Muhammad Awais <awaisoo668@gmail.com>
2025-10-24 22:29:09 +0500

    Add files via upload

---
+++ PF Lab/Quiz1/2502915_Quiz1.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+//-----------------------------------------------------------------------------
+
+// Q:1) Pritn your name
+
+/*int main (){
+    string name = "Muhammmad Awais";
+    cout<<name;    
+}*/
+
+//-----------------------------------------------------------------------------
+
+// Q:2) take 2 float number and  do  DMASM rule
+/*int main()
+{
+    float a, b, sum, diff, dev, mul;
+    int mod,c,d;
+    cout << "Enter two values :";
+    cin >> a >> b;
+    sum = a + b;
+    diff = a - b;
+    mul = a * b;
+    dev = a / b;
+    c = a;
+    d = b;
+    mod = c % d;
+    cout << "Sum is : " << sum << "\nSubtraction is: " << diff << "\nMultiplication is: " << mul << "\nDivision is: " << dev << "\nModulus is: " << mod;
+}*/
+
+//-----------------------------------------------------------------------------
+
+// Q:3) Take temp in celsius and onvert to fahrenheit
+
+/*int main(){
+    float  celsius,fahrenheit;
+    cout<<"Enter temperature in celsius : ";
+    cin>>celsius;
+    fahrenheit = (celsius * (9/5) + 32);
+    cout<<"Temperature in fahrenheit is : "<<fahrenheit<<" F";
+}*/
+
+//-----------------------------------------------------------------------------
+
+
+// Q:3) Print first letter of your name
+
+/*int main()
+{
+    cout << "                                                                         " << endl;
+    cout << "                                     **                                    " << endl;
+    cout << "                                    *  *                                   " << endl;
+    cout << "                                   *    *                                 " << endl;
+    cout << "                                  *      *                               " << endl;
+    cout << "                                 *        *                              " << endl;
+    cout << "                                *          *                              " << endl;
+    cout << "                               *            *                              " << endl;
+    cout << "                              * ************ *                             " << endl;
+    cout << "                             *                *                            " << endl;
+    cout << "                            *                  *                          " << endl;
+    cout << "                           *                    *                          " << endl;
+    cout << "                          *                      *                        " << endl;
+    cout << "                         *                        *                        " << endl;
+}*/
